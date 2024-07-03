5640c3a568a7d04de55a56c0953d84fb391395bc
Quan Le <quanle@Quans-MBP-2.attlocal.net>
2024-07-02 20:34:28 -0500

    added useState and default values to the inputs

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,24 +1,68 @@
+import { useState } from "react";
+
 export default function UserInput() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 100000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  }
+
   return (
     <section id="user-input">
       <div className="input-group">
         <p>
           <lable>Initial Investment</lable>
-          <input type="number" required />
+          <input
+            type="number"
+            value={userInput.initialInvestment}
+            required
+            onChange={(event) =>
+              handleChange("initialInvestment", event.target.value)
+            }
+          />
         </p>
         <p>
           <lable>Annual Investment</lable>
-          <input type="number" required />
+          <input
+            type="number"
+            value={userInput.annualInvestment}
+            required
+            onChange={(event) =>
+              handleChange("annualInvestment", event.target.value)
+            }
+          />
         </p>
       </div>
       <div className="input-group">
         <p>
           <lable>Expected Return</lable>
-          <input type="number" required />
+          <input
+            type="number"
+            value={userInput.expectedReturn}
+            required
+            onChange={(event) =>
+              handleChange("expectedReturn", event.target.value)
+            }
+          />
         </p>
         <p>
           <lable>Duration</lable>
-          <input type="number" required />
+          <input
+            type="number"
+            value={userInput.duration}
+            required
+            onChange={(event) => handleChange("duration", event.target.value)}
+          />
         </p>
       </div>
     </section>
